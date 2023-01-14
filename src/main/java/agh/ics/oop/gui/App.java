fac06a46021e213cc1444fc93ee5f8640c489111
Mpieta <maps321.mp@gmail.com>
2023-01-14 23:50:07 +0100

    Added Canvas GameScreen and CanvasElement classes

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -8,7 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.application.Application;
@@ -26,8 +27,28 @@ public class App extends Application {
         Timeline tl = new Timeline(new KeyFrame(Duration.millis((20)), e-> {gameScreen.run();}));
         tl.setCycleCount(Timeline.INDEFINITE);
         tl.play();
-        Canvas c = new Canvas(500,500);
-        primaryStage.setScene(new Scene(new StackPane(gameScreen.canvas)));
+
+        BorderPane bpane = new BorderPane();
+        bpane.setCenter(this.gameScreen.canvas);
+
+        Pane towers = new Pane();
+        towers.getChildren().add(new Label("Tower list (right pane)")); //mozliwe ze trzeba bdzie jakos przeniesc do gameScreen
+        bpane.setRight(towers);
+
+        Pane bottomPane = new Pane();
+        bottomPane.getChildren().add(new Label("Bottom Pane (info o obecnie wybranej wiezy/jednostce albo cos w tym stylu?)"));
+        bpane.setBottom(bottomPane);
+
+        Pane topPane = new Pane();
+        topPane.getChildren().add(new Label("moze HP/gold i jakies inne rzeczy(topPane)\n\n\n"));
+        bpane.setTop(topPane);
+
+        Pane leftPane = new Pane();
+        leftPane.getChildren().add(new Label("nwm co tu moze byc jeszcze, moze sie przyda (leftPane)"));
+        bpane.setLeft(leftPane);
+
+
+        primaryStage.setScene(new Scene(bpane));
         primaryStage.show();
     }
 }
